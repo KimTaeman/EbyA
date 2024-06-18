ef510d2bf9c40dda05de2f953c72473845cc7490
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2024-06-18 10:30:30 +0700

    chores

--- rootfolder/script.js
+++ rootfolder/script.js
@@ -1,12 +1,11 @@
 const rcmdCarousel = document.querySelector(".recommendations-carousel");
-console.log(rcmdCarousel.scrollLeft - 100)
 const rcmdItemWidth = document.querySelector(".recommendations-card").offsetWidth;
 const rcmdArrowBtns = document.querySelectorAll(".rcmd-btn");
 
 
 rcmdArrowBtns.forEach(btn => {
   btn.addEventListener("click", () => {
-    console.log(rcmdCarousel.scrollLeft += btn.id === "rcmd-left" ? -rcmdItemWidth : rcmdItemWidth);
+    rcmdCarousel.scrollLeft += btn.id === "rcmd-left" ? -rcmdItemWidth : rcmdItemWidth;
   })
 })
 
